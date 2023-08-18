560d0daa0e8d8ead51007b06eff5b1ebbe644314
Louis Weigel <account@louisweigel.com>
2023-08-18 09:48:28 +0200

    Fix some linting issues

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,37 +1,53 @@
 require 'open-uri'
 
+# docs :)
 class TeachersController < ApplicationController
   def index
-    url = URI.parse('https://www.elisabethschule.de/schulgemeinde/kollegium/')
-    document = Nokogiri::HTML.parse(URI.open(url))
-    table_data = document.css("table.contenttable tr:not(:first-child) td")
-    # teachers = []
+    teacher = current_teacher
+
+    if teacher == {}
+      head 404
+    else
+      render json: teacher
+    end
+  end
+
+  private
+
+  def parsed_table
+    document = Nokogiri::HTML.parse(URI.parse('https://www.elisabethschule.de/schulgemeinde/kollegium/').open)
+    document.css('table.contenttable tr:not(:first-child) td')
+  end
+
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
+  def current_teacher
+    table_data = parsed_table
 
     key = {
-      :name => 'name',
-      :abbreviation => 'abbreviation',
-      :subjects => 'subjects'
+      name: 'name',
+      abbreviation: 'abbreviation',
+      subjects: 'subjects'
     }
 
     current_key = key[:name]
 
     current_teacher = {}
 
-    table_data.each_with_index do |data, index|
-
-      if current_key === key[:name]
+    table_data.each_with_index do |data, _index|
+      if current_key == key[:name]
         current_teacher['name'] = data.text
         current_key = key[:abbreviation]
 
-      elsif current_key === key[:abbreviation]
+      elsif current_key == key[:abbreviation]
         current_teacher['abbreviation'] = data.text
         current_key = key[:subjects]
 
-      elsif current_key === key[:subjects]
+      elsif current_key == key[:subjects]
         current_teacher['subjects'] = data.text
         # teachers.append(current_teacher)
 
-        if current_teacher['abbreviation'] === params[:abbreviation]
+        if current_teacher['abbreviation'] == params[:abbreviation]
           # break and return the teacher
           break
         end
@@ -42,10 +58,6 @@ class TeachersController < ApplicationController
       # teachers.append(data.text)
     end
 
-    if current_teacher === {}
-      head 404
-    else
-      render json: current_teacher
-    end
+    current_teacher
   end
 end
